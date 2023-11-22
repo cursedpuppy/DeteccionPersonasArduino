009edc3a3ffbf5ea06f6c869f6f72a33a1d5d3ce
cursedpuppy <vicente.scheihing@gmail.com>
2023-11-21 23:47:05 -0300

    upsi movi el main

--- detectorPersonas.py
+++ detectorPersonas.py
@@ -12,8 +12,24 @@ from dotenv import load_dotenv
 from email.message import EmailMessage
 import ssl
 import smtplib
+import pyrebase
+from firebase_admin import credentials, initialize_app, firestore
 
+cred = credentials.Certificate("securitycameraia-firebase-adminsdk-h6reg-bc82044a45.json")
+initialize_app(cred, {'storageBucket': 'securitycameraia.appspot.com'})
 load_dotenv()
+firebaseConfig = {
+  "apiKey": "AIzaSyB2MlSOiq4ZB0ChuTytP7B12oouO13hx-A",
+  "authDomain": "securitycameraia.firebaseapp.com",
+  "projectId": "securitycameraia",
+  "storageBucket": "securitycameraia.appspot.com",
+  "messagingSenderId": "154010826465",
+  "appId": "1:154010826465:web:bcd4f05ecc7278afd569a0",
+  "measurementId": "G-FZET9WEE3H",
+  "serviceAccount":"securitycameraia-firebase-adminsdk-h6reg-bc82044a45.json",
+  "databaseURL":"gs://securitycameraia.appspot.com"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
 
 class SecurityApp:
     def __init__(self, root):
@@ -26,7 +42,6 @@ class SecurityApp:
         # Entrada para el correo
         self.email_label = tk.Label(root, text="Correo para notificaciones:")
         self.email_entry = tk.Entry(root, textvariable=self.email_var)
-        
 
         # Botón para iniciar la detección
         self.start_button = tk.Button(root, text="Iniciar Detección", command=self.start_detection)
@@ -35,6 +50,7 @@ class SecurityApp:
         self.email_label.pack(pady=10)
         self.email_entry.pack(pady=10)
         self.start_button.pack(pady=20)
+        self.root.geometry("200x200")
 
     def start_detection(self):
         # Obtener el correo desde la entrada
@@ -50,8 +66,6 @@ class SecurityApp:
         detection_thread.start()
 
     def run_detection(self, email_receiver):
-        # El código de detección original va aquí
-
         cap = cv2.VideoCapture(0)
         box_annotator = sv.BoxAnnotator(
             thickness=2,
@@ -98,7 +112,9 @@ class SecurityApp:
                 print("------------------Correo enviado xd---------------")
                 print("------------------Conteo reiniciada------------------")
                 person_count = 0
-                self.send_email(email_receiver)
+                image_url = self.upload_image_to_storage(frame)
+                self.send_email(email_receiver, image_url)
+                self.save_data_to_firestore(email_receiver, image_url)
 
             if cv2.waitKey(1000) == 27:
                 break
@@ -106,16 +122,41 @@ class SecurityApp:
         cap.release()
         cv2.destroyAllWindows()
 
+    def upload_image_to_storage(self, frame):
+        
+        firebase = pyrebase.initialize_app(firebaseConfig)
+        # Configurar el cliente de almacenamiento
+        storage = firebase.storage()
+
+        # Guardar la imagen localmente
+        file_path = "temp_image.jpg"
+        cv2.imwrite(file_path, frame)
+
+        # Subir la imagen al almacenamiento
+        cloudfilename = f"images/{int(time.time())}.jpg"
+        storage.child(cloudfilename).put(file_path)
+
+        # Obtener la URL de la imagen
+        url = storage.child(cloudfilename).get_url(None)
+
+        # Eliminar el archivo local
+        os.remove(file_path)
+
+        return url
+
+
     def count_people(self, detections, model):
         return sum(1 for _, confidence, class_id, _ in detections if model.model.names[class_id] == "person")
 
-    def send_email(self, email_receiver):
+    def send_email(self, email_receiver, image_url):
         em = EmailMessage()
         em["From"] = "losshupaia@gmail.com"
         em["To"] = email_receiver
         em["Subject"] = "Aviso cámara de seguridad!"
-        em.set_content("""
+        em.set_content(f"""
             Se ha visto a una persona sospechosa en el establecimiento. Tomar precaución.
+
+            Imagen de la persona: {image_url}
         """)
 
         context = ssl.create_default_context()
@@ -124,9 +165,20 @@ class SecurityApp:
             smtp.login("losshupaia@gmail.com", os.getenv("PASSWORD"))
             smtp.sendmail("losshupaia@gmail.com", email_receiver, em.as_string())
 
+    def save_data_to_firestore(self, email_receiver, image_url):
+        detections_ref = firestore.client().collection('detections')
+
+        detections_ref.add({
+            'timestamp': int(time.time()),
+            'correo': email_receiver,
+            'imageUrl': image_url,
+        })
+
+
 def main():
     root = tk.Tk()
     app = SecurityApp(root)
+
     root.mainloop()
 
 if __name__ == "__main__":
---
+++ supervisioRoutea.py
@@ -0,0 +1,3 @@
+import supervision as sv
+
+print("Ruta de supervisión:", sv.__file__)
